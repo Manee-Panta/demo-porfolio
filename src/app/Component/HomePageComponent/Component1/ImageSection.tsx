14b5d187a2705b495f4873ba34b9c978ba62d309
panta-manee <pantamanee145@gmail.com>
2024-10-20 18:47:47 +0545

    style check

--- src/app/Component/HomePageComponent/Component1/ImageSection.tsx
+++ src/app/Component/HomePageComponent/Component1/ImageSection.tsx
@@ -5,12 +5,13 @@ export function ImageSection() {
     <div className="relative lg:w-1/2">
       <div className="w-64 md:w-80 lg:w-100 h-64 md:h-80 lg:h-96 bg-gradient-to-r from-yellow-100 via-purple-100 to-pink-200 rounded-full flex items-center justify-center mx-auto">
         <Image
-          src="/Manita_Panta.webp " // Update with actual path to the profile image
+          src="/Manita_Panta.webp" // Update with actual path to the profile image
           width={200}
           height={250}
           alt="Profile Picture"
           className="rounded-full"
           loading="lazy"
+          style={{border: "5px solid red"}}
         />
       </div>
     </div>
